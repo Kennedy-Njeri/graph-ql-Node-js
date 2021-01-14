5f3cf4b9a3938a532898affee9f04f3a76c93230
Kennedy Njeri <mistakenz123@gmail.com>
2021-01-14 20:55:35 +0300

    config node js in prisma

--- graphql-prisma/src/index.js
+++ graphql-prisma/src/index.js
@@ -1,7 +1,7 @@
 import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
 import Query from './resolvers/Query'
-import Comment from './resolvers/Comment'
+import Comment from './Comment'
 import Mutation from './resolvers/Mutation'
 import Post from './resolvers/Post'
 import User from './resolvers/User'
